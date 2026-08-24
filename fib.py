c06ae439f879e2deed54bcfcb5effba5dc6b3659
Nikatine <11243407+Nikatine@users.noreply.github.com>
2026-08-23 23:06:10 -0700

    fibonnaci sequence generator

---
+++ fib.py
@@ -0,0 +1,9 @@
+def fibonacci_with_list(n):
+    fib_series = [0, 1]
+    for i in range (2, n):
+        fib_series.append(fib_series[-1] + fib_series[-2])
+    return fib_series
+
+n = 15
+result = fibonacci_with_list(n)
+print("Fibonacci series with ", n, "elements: ", result)
