d2187eece7ae3adee4544d25530847ae0e7331b1
Nikatine <11243407+Nikatine@users.noreply.github.com>
2026-08-23 23:17:48 -0700

    fib seq w/ vars

--- fib.py
+++ fib.py
@@ -1,3 +1,5 @@
+'''
+#List
 def fibonacci_with_list(n):
     fib_series = [0, 1]
     for i in range (2, n):
@@ -7,3 +9,15 @@ def fibonacci_with_list(n):
 n = 15
 result = fibonacci_with_list(n)
 print("Fibonacci series with ", n, "elements: ", result)
+'''
+
+#Variables
+def fibonacci_with_vars(n):
+    a, b = 0, 1
+    for _ in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+
+n = 10
+print(f"Fibonacci series with {n} values: ")
+fibonacci_with_vars(n)
